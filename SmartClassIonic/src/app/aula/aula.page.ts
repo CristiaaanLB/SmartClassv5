7f0ab08e3fe5d6ce00526d252ab8f322c978d179
CristiaaanLB <cristian.lb.2002@gmail.com>
2022-08-07 20:29:51 -0700

    prepared aula page

--- SmartClassIonic/src/app/aula/aula.page.ts
+++ SmartClassIonic/src/app/aula/aula.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IonRouterOutlet } from '@ionic/angular';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-aula',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AulaPage implements OnInit {
 
-  constructor() { }
+  id;
+
+  constructor(private routerOutlet: IonRouterOutlet, private route: ActivatedRoute) { }
+
+  goBack() {
+    this.routerOutlet.pop();
+  }
 
   ngOnInit() {
   }
 
+  ionViewWillEnter(){
+    this.route.paramMap.subscribe(params => {
+      this.id = params.get('id');
+    });
+
+    console.log(this.id);
+  }
 }
